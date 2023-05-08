d323fb78de0252f3a7676699742dd392c876d3ee
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-05-08 16:54:27 +0800

    Update Nets.py

--- fedavg/models/Nets.py
+++ fedavg/models/Nets.py
@@ -30,7 +30,7 @@ class CNNMnist(nn.Module):
         self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(320, 50)  #第一個參數:输入的神经元个数,第二個參數:输出的神经元个数
         self.fc2 = nn.Linear(50, args.num_classes)
 
     def forward(self, x):
